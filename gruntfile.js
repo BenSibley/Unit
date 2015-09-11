03c14d8a56a8a40dd067e9593d11355a81aa6243
BenSibley <ben@competethemes.com>
2015-09-11 15:29:32 -0400

    added grunt command for publishing theme

--- gruntfile.js
+++ gruntfile.js
@@ -59,6 +59,22 @@ module.exports = function(grunt) {
                     type: 'wp-theme'
                 }
             }
+        },
+        excludeFiles: '--exclude "*.gitignore" --exclude ".sass-cache/" --exclude "*.DS_Store" --exclude ".git/" --exclude ".idea/" --exclude "gruntfile.js" --exclude "node_modules/" --exclude "package.json" --exclude "sass/"',
+        shell: {
+            zip: {
+                command: [
+                    // delete existing copies on Desktop (if they exist)
+                    'rm -R /Users/bensibley/Desktop/unit || true',
+                    'rm -R /Users/bensibley/Desktop/unit.zip || true',
+                    // copy theme folder to desktop without any project/meta files
+                    'rsync -r /Applications/MAMP/htdocs/wordpress/wp-content/themes/unit /Users/bensibley/Desktop/ <%= excludeFiles %>',
+                    // open desktop
+                    'cd /Users/bensibley/Desktop/',
+                    // zip the unit folder on desktop
+                    'zip -r unit.zip unit'
+                ].join('&&')
+            }
         }
     });
 
@@ -69,8 +85,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-wp-i18n');
     grunt.loadNpmTasks('grunt-cssjanus');
+    grunt.loadNpmTasks('grunt-shell');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['watch', 'sass', 'autoprefixer', 'cssmin', 'makepot', 'cssjanus']);
+    grunt.registerTask('default', ['watch', 'sass', 'autoprefixer', 'cssmin', 'makepot', 'cssjanus', 'shell']);
 
 };
